34e06f1007fceb03c43c0518e02cb3594b0844c9
hafizsyedirfanali <hafizsyedirfanali@gmail.com>
2025-08-12 14:01:34 +0530

    update

--- CSharpBasics/obj/Debug/net8.0/CSharpBasics.AssemblyInfo.cs
+++ CSharpBasics/obj/Debug/net8.0/CSharpBasics.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSharpBasics")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34f105669e86687e7adbde20aee205421b5019a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSharpBasics")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSharpBasics")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
