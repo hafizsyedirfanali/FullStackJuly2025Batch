b3654c218366306cd0427225b3972dd19c0848ba
Noor <ansarinoorafshan14@gmail.com>
2025-08-29 20:26:25 +0530

    update

---
+++ ObjectOrientedProgramming/T01_Classes.cs
@@ -0,0 +1,18 @@
+﻿namespace ObjectOrientedProgramming;
+/// <summary>
+/// A class is a collection of class members like
+/// 1. Properties
+/// 2. Fields
+/// 3. Constructors - Always present
+/// 4. Methods
+/// 5. Class
+/// 6. Struct
+/// 7. Destructor - Always present
+/// </summary>
+public class T01_Classes
+{
+    public static void Test()//Method
+    {
+
+    }
+}
