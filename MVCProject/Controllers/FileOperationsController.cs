83228c3e0ea8b843fbef6328493ae4cfb90ea819
Noor <ansarinoorafshan14@gmail.com>
2025-10-04 00:39:41 +0530

    update

---
+++ MVCProject/Controllers/FileOperationsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCProject.Controllers
+{
+    public class FileOperationsController : Controller
+    {
+        private readonly IWebHostEnvironment webHostEnvironment;
+
+        public FileOperationsController(IWebHostEnvironment webHostEnvironment)
+        {
+            this.webHostEnvironment = webHostEnvironment;
+        }
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult FileSave(IFormFile file)
+        {
+            var folderPath = Path.Combine(webHostEnvironment.ContentRootPath, "wwwroot", "Uploads");
+            if (!Directory.Exists(folderPath))
+            {
+                Directory.CreateDirectory(folderPath);
+            }
+            var fileName = Guid.NewGuid() + "_resume.pdf";
+            var filePath = Path.Combine(folderPath, fileName);
+            using (var fileStream = new FileStream(filePath, FileMode.Create))
+            {
+                file.CopyTo(fileStream);
+            }
+            //return RedirectToAction("Index");
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
--- MVCProject/Controllers/HomeController.cs
+++ MVCProject/Controllers/HomeController.cs
@@ -77,6 +77,34 @@ public class HomeController : Controller
         //handle invalid data
         return View(model);
     }
+    [HttpGet]
+    public IActionResult ModelBindingList()
+    {
+        List<StudentModel> students = new List<StudentModel>
+        {
+            new StudentModel { Name = "Abid", Age = 10, Address = "Nagpur" },
+            new StudentModel { Name = "Sara", Age = 11, Address = "Mumbai" },
+            new StudentModel { Name = "Ravi", Age = 12, Address = "Delhi" },
+            new StudentModel { Name = "Priya", Age = 10, Address = "Pune" },
+            new StudentModel { Name = "Amit", Age = 13, Address = "Hyderabad" },
+            new StudentModel { Name = "Neha", Age = 11, Address = "Chennai" },
+            new StudentModel { Name = "Karan", Age = 12, Address = "Bangalore" },
+            new StudentModel { Name = "Meena", Age = 10, Address = "Kolkata" },
+            new StudentModel { Name = "Vikram", Age = 14, Address = "Ahmedabad" },
+            new StudentModel { Name = "Anjali", Age = 13, Address = "Jaipur" }
+        };
+        return View(students);
+    }
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult ModelBindingList(List<StudentModel> students)
+    {
+        if (ModelState.IsValid)
+        {
+            //save in db
+        }
+        return View(students);
+    }
     public IActionResult Error()
     {
         return Ok("");
